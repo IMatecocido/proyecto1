a622de9affa2d0b9ec2fcb2d910c2861fef49c18
Camilo Rodriguez <camilotrep@gmail.com>
2025-04-15 03:59:26 -0300

    epic debug

--- New Unity Project/Assets/Scripts/HolaMundo.cs
+++ New Unity Project/Assets/Scripts/HolaMundo.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+
+    int x;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hola mundo");
+        x = 0;
+
+        print("algo salio mal");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        x = x + 1;
+        Debug.Log(x);
+
     }
 }
